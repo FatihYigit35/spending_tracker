0cb409cbac21b292ce81df39282b5cd1968fc60f
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-06-22 00:50:47 +0300

    added AppBar and SpendingList

--- lib/view/spendings_screen.dart
+++ lib/view/spendings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:spending_tracker/model/spending.dart';
+import 'package:spending_tracker/widget/spending_list.dart';
 
 class SpendingsScreen extends StatefulWidget {
   const SpendingsScreen({super.key});
@@ -8,14 +10,42 @@ class SpendingsScreen extends StatefulWidget {
 }
 
 class _SpendingsScreenState extends State<SpendingsScreen> {
+  final List<Spending> _registeredSpendings = [
+    Spending(
+      title: 'Kitap',
+      amount: 20.8,
+      date: DateTime.now(),
+      category: Category.shopping,
+      description: 'Doctor Who - 11 Doktor 11 Öykü kitabu alındı',
+    ),
+    Spending(
+      title: 'Sinema',
+      amount: 30.5,
+      date: DateTime.now(),
+      category: Category.entertainment,
+      description: 'Matrix filmine gidildi',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            ),
+          ],
+          backgroundColor: Colors.green.shade200,
+        ),
         body: Column(
-      children: [
-        Text("Spendings chart"),
-        Text("Spendings list"),
-      ],
-    ));
+          children: [
+            const Text("Spendings chart"),
+            Expanded(
+              child: SpendingList(spendings: _registeredSpendings),
+            ),
+          ],
+        ));
   }
 }
