edfd2b9de8f4ce330c7fe889dc6b2d2fd97859a2
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-06-22 00:51:29 +0300

    create spending card widget

---
+++ lib/widget/spending_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:spending_tracker/model/spending.dart';
+
+class SpendingCard extends StatelessWidget {
+  const SpendingCard({super.key, required this.spending});
+
+  final Spending spending;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        child: Column(
+          children: [
+            Text(
+              spending.title,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              spending.description,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Row(
+                  children: [
+                    Text(categoryIcons[spending.category].toString()),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      spending.formatedDate,
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Text(
+                  '₺${spending.amount.toStringAsFixed(2)}',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
