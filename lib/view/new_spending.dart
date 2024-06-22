fd0e49de1a540f7724435aa4252f1238e11417cf
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-06-23 02:26:16 +0300

    Required items to enter new expenses have been adjusted

--- lib/view/new_spending.dart
+++ lib/view/new_spending.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:spending_tracker/model/spending.dart';
 
 class NewSpending extends StatefulWidget {
-  const NewSpending({super.key});
+  const NewSpending({super.key, required this.onAddSpending});
+
+  final void Function(Spending spending) onAddSpending;
 
   @override
   State<NewSpending> createState() => _NewSpendingState();
@@ -9,15 +12,16 @@ class NewSpending extends StatefulWidget {
 
 class _NewSpendingState extends State<NewSpending> {
   var dateNow = DateTime.now();
-  String selectedDate = '';
+  var _selectedDate = '';
   final _titleController = TextEditingController();
   final _descriptionController = TextEditingController();
   final _amountController = TextEditingController();
+  var _selectedCategory = Category.shopping;
 
   @override
   void initState() {
     super.initState();
-    selectedDate = '${dateNow.day}/${dateNow.month}/${dateNow.year}';
+    _selectedDate = '${dateNow.day}/${dateNow.month}/${dateNow.year}';
   }
 
   @override
@@ -29,17 +33,84 @@ class _NewSpendingState extends State<NewSpending> {
     super.dispose();
   }
 
-  void _openDatePicker() {
+  void _openDatePicker() async {
     final firstDate = DateTime(
       dateNow.year - 1,
       dateNow.month,
       dateNow.day,
     );
-    showDatePicker(
+    final pickedDate = await showDatePicker(
       context: context,
       firstDate: firstDate,
       lastDate: dateNow,
     );
+
+    setState(() {
+      if (pickedDate != null) {
+        _selectedDate =
+            '${pickedDate.day}/${pickedDate.month}/${pickedDate.year}';
+
+        dateNow = pickedDate;
+      }
+    });
+  }
+
+  void _submitSpendingData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+
+    if (enteredAmount == null || enteredAmount <= 0) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          actions: [
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+          title: const Text('Missing Value!'),
+          content: const Text('Please enter amount.'),
+        ),
+      );
+      return;
+    }
+
+    if (_titleController.text.trim().isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          actions: [
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+          title: const Text('Missing Value!'),
+          content: const Text('Please enter title.'),
+        ),
+      );
+      return;
+    }
+
+    final enteredTitle = _titleController.text;
+    final enteredDescription = _descriptionController.text;
+    final enteredDate = dateNow;
+
+    final newSpending = Spending(
+      amount: enteredAmount,
+      title: enteredTitle,
+      description: enteredDescription,
+      date: enteredDate,
+      category: _selectedCategory,
+    );
+
+    widget.onAddSpending(newSpending);
+
+    Navigator.pop(context);
   }
 
   @override
@@ -80,18 +151,51 @@ class _NewSpendingState extends State<NewSpending> {
                 width: 20,
               ),
               Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                child: Column(
                   children: [
-                    Text(selectedDate),
-                    IconButton(
-                      onPressed: _openDatePicker,
-                      icon: const Icon(Icons.calendar_month),
-                    )
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(_selectedDate),
+                        IconButton(
+                          onPressed: _openDatePicker,
+                          icon: const Icon(Icons.calendar_month),
+                        )
+                      ],
+                    ),
                   ],
                 ),
               )
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value != null) {
+                      setState(() {
+                        _selectedCategory = value;
+                      });
+                    }
+                  }),
             ],
           ),
           const SizedBox(
@@ -107,7 +211,7 @@ class _NewSpendingState extends State<NewSpending> {
                 child: const Text('Close'),
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _submitSpendingData,
                 child: const Text('Save'),
               ),
             ],
